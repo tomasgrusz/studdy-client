349e63cb5377623a2767f9c950a1dee80e072425
Tomas Grusz <grusztomas0@gmail.com>
2022-10-27 15:24:43 +0100

    Initial create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import './App.css'
+import Three from './components/three';
+import { FlashcardCreator } from './components/flashcardCreator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <FlashcardCreator></FlashcardCreator>
+      {/* <Canvas id="three-canvas-container" shadows>
+        <Suspense fallback={<></>}>
+          <Three />
+        </Suspense>
+      </Canvas> */}
+    </>
+  )
 }
 
-export default App;
+export default App
