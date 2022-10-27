7951783d0618606493f02173c3e3f158d475fcc8
Tomas Grusz <grusztomas0@gmail.com>
2022-10-27 15:25:53 +0100

    Create flashcardCreator component

---
+++ src/components/flashcardCreator/index.js
@@ -0,0 +1,111 @@
+import React, { useRef, useState } from 'react';
+import { EditorState, RichUtils } from 'draft-js';
+import Editor from 'draft-js-plugins-editor'
+import { stateToHTML } from 'draft-js-export-html'
+import { stateFromHTML } from 'draft-js-import-html';
+import Axios from 'axios';
+
+import 'draft-js/dist/Draft.css';
+import './index.css';
+
+const highlightPlugin = {
+    customStyleMap: {
+        HIGHLIGHT: {
+            background: "#fffe0d"
+        }
+    },
+    keyBindingFn: e => {
+        if (e.metaKey && e.key === "h") {
+            return "highlight";
+        }
+    },
+    handleKeyCommand: (command, editorState, { setEditorState }) => {
+        if (command === "highlight") {
+            setEditorState(RichUtils.toggleInlineStyle(editorState, "HIGHLIGHT"));
+            return true;
+        }
+    }
+}
+
+const FlashcardCreator = () => {
+
+    const [title, setTitle] = useState('');
+    const [editorState, setEditorState] = React.useState(() => EditorState.createEmpty())
+    const [editorState2, setEditorState2] = React.useState(() => EditorState.createEmpty())
+    const editorRef = useRef(null)
+    const editorRef2 = useRef(null)
+
+    const printTitle = event => {
+        event.preventDefault();
+        console.log(title)
+    }
+
+    const onHighlight = () => {
+        setEditorState(
+            RichUtils.toggleInlineStyle(editorState, "HIGHLIGHT")
+        );
+    };
+
+    const options = {
+        inlineStyles: {
+            HIGHLIGHT: {
+                style: { background: "#fffe0d" },
+                class: 'hiddenText'
+            }
+        }
+    }
+
+    const duplicateEditor = () => {
+        const html = stateToHTML(editorState.getCurrentContent(), options)
+        console.log(html)
+        setEditorState2(EditorState.createWithContent(stateFromHTML(html)))
+        console.log(stateToHTML(editorState2.getCurrentContent()))
+    }
+
+    const queryDB = async () => {
+        try {
+            await Axios.get(
+                "http://localhost:3001/",
+                {
+                    withCredentials: true,
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((response) => {
+                    console.log(response.data[0])
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return (
+        <>
+            <div className='form-div'>
+                <input
+                    className='textfield'
+                    required
+                    id="form-flaschard-title"
+                    placeholder="Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <div>
+                    <button className="highlight" onClick={onHighlight}>Highlight</button>
+                    <Editor editorState={editorState} onChange={setEditorState} placeholder="Card description" plugins={[highlightPlugin]} ref={editorRef} />
+                </div>
+                <input type='button' value='Submit' onClick={printTitle} />
+            </div>
+            <button onClick={duplicateEditor}>Duplicate</button>
+            <Editor editorState={editorState2} onChange={setEditorState2} placeholder="Card description" ref={editorRef2} />
+            <button onClick={queryDB}>Query DB</button>
+        </>
+    )
+};
+
+export { FlashcardCreator };
